9ff31e4400674367b4d68afe4c7e74624f4295fd
Deacon <deacon@raygun.com>
2022-04-22 12:12:10 +1200

    Add MonogameView, with simple dragging

---
+++ MonoGameView/Draggable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameView
+{
+    public class Draggable
+    {
+        private Vector2 _position;
+        private int _height;
+        private int _width;
+        private Texture2D _texture;
+        private bool _beingDragged = false;
+        private float _xOffsetFromMouse;
+        private float _yOffsetFromMouse;
+
+        public Draggable(Vector2 position, int height, int width, Texture2D texture)
+        {
+            _position = position;
+            _height = height;
+            _width = width;
+            _texture = texture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, new Rectangle((int)_position.X, (int)_position.Y, _width, _height), Color.White);
+        }
+
+        public void Update(MouseState mouseState)
+        {
+            if (_beingDragged)
+            {
+                _position = new Vector2(mouseState.X - _xOffsetFromMouse, mouseState.Y - _yOffsetFromMouse);
+            }
+        }
+
+        public bool MouseInBounds(Point mousePosition)
+        {
+            return mousePosition.X > _position.X &&
+                   mousePosition.X < _position.X + _width &&
+                   mousePosition.Y > _position.Y &&
+                   mousePosition.Y < _position.Y + _height;
+        }
+
+        public void Grab(MouseState mouseState)
+        {
+            _beingDragged = true;
+            _xOffsetFromMouse = mouseState.X - _position.X;
+            _yOffsetFromMouse = mouseState.Y - _position.Y;
+        }
+
+        public void Release()
+        {
+            _beingDragged = false;
+        }
+    }
+}
---
+++ MonoGameView/Game1.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameView;
+
+namespace MonoGameCross_PlatformDesktopApplication1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _ballTexture;
+        private Vector2 _ballPosition;
+        private float _ballSpeed;
+        private List<Draggable> _draggables;
+        private Draggable _draggedItem;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _draggables = new List<Draggable>();
+        }
+
+        protected override void Initialize()
+        {
+            _ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                _graphics.PreferredBackBufferHeight / 2);
+            _ballSpeed = 1000f;
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var blankTexture = new Texture2D(GraphicsDevice, 1, 1);
+            blankTexture.SetData(new[] { Color.DarkSlateGray });
+            for (int i = 50; i < 300; i+=25)
+            {
+                _draggables.Add(new Draggable(new Vector2(i, _graphics.PreferredBackBufferHeight / 2), 20, 20, blankTexture));
+            }
+
+            _ballTexture = Content.Load<Texture2D>("ball");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var kstate = Keyboard.GetState();
+            var mouseState = Mouse.GetState();
+            
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                kstate.IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (_draggedItem == null && mouseState.LeftButton == ButtonState.Pressed)
+            {
+                foreach (var draggable in _draggables)
+                {
+                    if (draggable.MouseInBounds(mouseState.Position))
+                    {
+                        _draggedItem = draggable;
+                        draggable.Grab(mouseState);
+                        break;
+                    }
+                }
+            }
+            
+            if (mouseState.LeftButton == ButtonState.Released)
+            {
+                if (_draggedItem != null)
+                {
+                    _draggedItem.Release();
+                    _draggedItem = null;
+                }
+            }
+            
+            _draggables.ForEach(d => d.Update(mouseState));
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _draggables.ForEach(d => d.Draw(_spriteBatch));
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
