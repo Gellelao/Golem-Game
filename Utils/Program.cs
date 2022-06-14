db91bd30c97b874183e27f6b0050b23426b48f5d
Deacon <deacon@raygun.com>
2022-06-14 19:01:19 +1200

    Delete GolemApp project

--- Utils/Program.cs
+++ Utils/Program.cs
@@ -1,5 +1,80 @@
 ﻿using static Utils.PartsManager;
+using System.Text.Json;
+using GolemCore;
+using GolemCore.Models;
+using GolemCore.Models.Golem;
 
 Console.WriteLine("Hello, World!");
 
 await WritePartsFromDatabaseIntoFile("test.json");
+
+
+
+Console.WriteLine("Hello, World!");
+
+var client = GolemApiClientFactory.Create();
+
+var cancellationToken = new CancellationToken();
+
+var golem1 = new Golem
+{
+    UserId = 1,
+    PartIds = new []
+    {
+        new []{"1", "1", "1"},
+        new []{"0", "0", "0"},
+        new []{"1", "0", "1"},
+    }
+};
+
+var golem2 = new Golem
+{
+    UserId = 2,
+    PartIds = new []
+    {
+        new []{"0", "1", "0"},
+        new []{"0", "0", "0"},
+        new []{"0", "0", "0"},
+    }
+};
+
+var parts = await client.GetParts(cancellationToken);
+
+var partsCache = new PartsCache(parts);
+
+var results = Resolver.GetOutcome(golem1, golem2, partsCache);
+
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
+
+var shop = new Shop(partsCache);
+
+await client.CreateGolem(new CreateGolemRequest{Item = golem1}, cancellationToken);
+
+var golems = await client.GetGolemSelection(new CancellationToken());
+
+Console.WriteLine(golems[0].UserId);
+
+var golemToUpdate = golems.First(g => g.Version == 1);
+
+var updateRequest = new UpdateRequest
+{
+    TableName = "golem",
+    Key = new
+    {
+        Timestamp = golemToUpdate.Timestamp,
+        UserId = golemToUpdate.UserId
+    },
+    UpdateExpression = "set Version = :newVersion",
+    ConditionExpression = "Version = :version",
+    ExpressionAttributeValues = new Dictionary<string, object>
+    {
+        { ":newVersion", 0 },
+        { ":version", 1 }
+    }
+};
+
+var json = JsonSerializer.Serialize(updateRequest, new JsonSerializerOptions(new JsonSerializerOptions{PropertyNamingPolicy = null}));
+await client.UpdateGolem(updateRequest, cancellationToken);
