46f8bb547c73db2e4004b26e8a178bc827fa9a32
Deacon <deacon@raygun.com>
2022-08-12 08:37:16 +1200

    Move Part and Stat extension methods onto models themselves

--- GolemCore/GolemCore/Models/Part/Part.cs
+++ GolemCore/GolemCore/Models/Part/Part.cs
@@ -1,4 +1,5 @@
-﻿using GolemCore.Models.Enums;
+﻿using System.Text;
+using GolemCore.Models.Enums;
 using GolemCore.Models.Triggers;
 
 namespace GolemCore.Models.Part;
@@ -19,4 +20,33 @@ public class Part
     new []{false, false, false, false},
     new []{false, false, false, false}
   };
+  
+  public string GetDescription()
+  {
+    var builder = new StringBuilder();
+    foreach (var stat in Stats)
+    {
+      builder.AppendLine($"{(stat.Modifier > 0 ? "+" : "")}{stat.Modifier} {stat.Type.ToString()}");
+      if (stat.StatMultiplier != null)
+      {
+        builder.AppendLine($"For each {stat.StatMultiplier}");
+      }
+      if (stat.Condition != null)
+      {
+        builder.AppendLine($"Only if this part has {stat.Condition}");
+      }
+    }
+
+    foreach (var tag in Tags)
+    {
+      builder.AppendLine(tag.ToString());
+    }
+        
+    foreach (var trigger in Triggers)
+    {
+      builder.AppendLine(trigger.ToString());
+    }
+
+    return builder.ToString();
+  }
 }
--- GolemCore/GolemCore/Models/Stat.cs
+++ GolemCore/GolemCore/Models/Stat.cs
@@ -10,4 +10,19 @@ public class Stat
   public NeighbourhoodRequirement? Condition { get; init; }
   
   public Neighbourhood? StatMultiplier { get; init; }
+  
+  public int CalculateTotalModifier(string fullPartId, Golem.Golem golem, PartsCache cache)
+  {
+    if (Condition != null && !Condition.Satisfied(fullPartId, golem, cache))
+    {
+      return 0;
+    }
+
+    if (StatMultiplier != null)
+    {
+      return Modifier * StatMultiplier.MatchingNeighboursCount(fullPartId, golem, cache);
+    }
+
+    return Modifier;
+  }
 }
